a32622fbbcdea81ac6b9cf1706caf2faa8103fc4
Robert <robert@space>
2024-10-11 12:00:43 +0200

    comment added

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Clear previously generated libraries and create new
+# Needs to be in venv!!!
 python3 setup.py clean --all
 rm -rf build evolutionary/*.so evolutionary/*.c
 python setup.py build_ext --inplace

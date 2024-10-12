2e4ad386a7a351727f0a48f24dadfd4c22bdcf03
Robert <robert@space>
2024-10-13 01:09:20 +0200

    classes 5X added

--- config_gen.py
+++ config_gen.py
@@ -153,6 +153,34 @@ def get_config() -> Config:
                 "id": 37,
                 "name": "GoKa"
             },
+            {
+                "id": 38,
+                "name": "PaPa"
+            },
+            {
+                "id": 39,
+                "name": "MiMa"
+            },
+            {
+                "id": 40,
+                "name": "BaKa"
+            },
+            {
+                "id": 41,
+                "name": "ŻeWi"
+            },
+            {
+                "id": 42,
+                "name": "LaPi"
+            },
+            {
+                "id": 43,
+                "name": "KoOl"
+            },
+            {
+                "id": 44,
+                "name": "CyJa"
+            },
         ],
         subjects={
             "4A": [
@@ -497,6 +525,378 @@ def get_config() -> Config:
                     "hours": 2,
                     "teacher_id": 3,
                     "start_end": True
+                },
+            ],
+            "5A": [
+                {
+                    "id": 1,
+                    "name": "J. Polski",
+                    "hours": 5,
+                    "teacher_id": 27,
+                    "start_end": False
+                },
+                {
+                    "id": 2,
+                    "name": "Historia",
+                    "hours": 2,
+                    "teacher_id": 38,
+                    "start_end": False
+                },
+                {
+                    "id": 3,
+                    "name": "Technika",
+                    "hours": 1,
+                    "teacher_id": 23,
+                    "start_end": False
+                },
+                {
+                    "id": 4,
+                    "name": "Matematyka",
+                    "hours": 5,
+                    "teacher_id": 28,
+                    "start_end": False
+                },
+                {
+                    "id": 5,
+                    "name": "WF",
+                    "hours": 4,
+                    "teacher_id": 19,
+                    "start_end": False
+                },
+                {
+                    "id": 6,
+                    "name": "Muzyka",
+                    "hours": 1,
+                    "teacher_id": 24,
+                    "start_end": False
+                },
+                {
+                    "id": 7,
+                    "name": "Plastyka",
+                    "hours": 1,
+                    "teacher_id": 30,
+                    "start_end": False
+                },
+                {
+                    "id": 8,
+                    "name": "Religia",
+                    "hours": 2,
+                    "teacher_id": 5,
+                    "start_end": True
+                },
+                {
+                    "id": 9,
+                    "name": "Informatyka",
+                    "hours": 1,
+                    "teacher_id": 18,
+                    "start_end": False
+                },
+                {
+                    "id": 10,
+                    "name": "J. Angielski",
+                    "hours": 2,
+                    "teacher_id": 20,
+                    "start_end": False
+                },
+                {
+                    "id": 11,
+                    "name": "Geografia",
+                    "hours": 1,
+                    "teacher_id": 39,
+                    "start_end": False
+                },
+                {
+                    "id": 12,
+                    "name": "Biologia",
+                    "hours": 1,
+                    "teacher_id": 37,
+                    "start_end": False
+                },
+                {
+                    "id": 13,
+                    "name": "G. Wychowawcza",
+                    "hours": 1,
+                    "teacher_id": 28,
+                    "start_end": False
+                },
+            ],
+            "5B": [
+                {
+                    "id": 1,
+                    "name": "J. Polski",
+                    "hours": 5,
+                    "teacher_id": 40,
+                    "start_end": False
+                },
+                {
+                    "id": 2,
+                    "name": "Historia",
+                    "hours": 2,
+                    "teacher_id": 22,
+                    "start_end": False
+                },
+                {
+                    "id": 3,
+                    "name": "Technika",
+                    "hours": 1,
+                    "teacher_id": 23,
+                    "start_end": False
+                },
+                {
+                    "id": 4,
+                    "name": "Matematyka",
+                    "hours": 5,
+                    "teacher_id": 28,
+                    "start_end": False
+                },
+                {
+                    "id": 5,
+                    "name": "WF",
+                    "hours": 4,
+                    "teacher_id": 41,
+                    "start_end": False
+                },
+                {
+                    "id": 6,
+                    "name": "Muzyka",
+                    "hours": 1,
+                    "teacher_id": 24,
+                    "start_end": False
+                },
+                {
+                    "id": 7,
+                    "name": "Plastyka",
+                    "hours": 1,
+                    "teacher_id": 30,
+                    "start_end": False
+                },
+                {
+                    "id": 8,
+                    "name": "Religia",
+                    "hours": 2,
+                    "teacher_id": 5,
+                    "start_end": True
+                },
+                {
+                    "id": 9,
+                    "name": "Informatyka",
+                    "hours": 1,
+                    "teacher_id": 31,
+                    "start_end": False
+                },
+                {
+                    "id": 10,
+                    "name": "J. Angielski",
+                    "hours": 2,
+                    "teacher_id": 9,
+                    "start_end": False
+                },
+                {
+                    "id": 11,
+                    "name": "Geografia",
+                    "hours": 1,
+                    "teacher_id": 39,
+                    "start_end": False
+                },
+                {
+                    "id": 12,
+                    "name": "Biologia",
+                    "hours": 1,
+                    "teacher_id": 37,
+                    "start_end": False
+                },
+                {
+                    "id": 13,
+                    "name": "G. Wychowawcza",
+                    "hours": 1,
+                    "teacher_id": 22,
+                    "start_end": False
+                },
+            ],
+            "5C": [
+                {
+                    "id": 1,
+                    "name": "J. Polski",
+                    "hours": 5,
+                    "teacher_id": 33,
+                    "start_end": False
+                },
+                {
+                    "id": 2,
+                    "name": "Historia",
+                    "hours": 2,
+                    "teacher_id": 38,
+                    "start_end": False
+                },
+                {
+                    "id": 3,
+                    "name": "Technika",
+                    "hours": 1,
+                    "teacher_id": 23,
+                    "start_end": False
+                },
+                {
+                    "id": 4,
+                    "name": "Matematyka",
+                    "hours": 5,
+                    "teacher_id": 42,
+                    "start_end": False
+                },
+                {
+                    "id": 5,
+                    "name": "WF",
+                    "hours": 4,
+                    "teacher_id": 18,
+                    "start_end": False
+                },
+                {
+                    "id": 6,
+                    "name": "Muzyka",
+                    "hours": 1,
+                    "teacher_id": 24,
+                    "start_end": False
+                },
+                {
+                    "id": 7,
+                    "name": "Plastyka",
+                    "hours": 1,
+                    "teacher_id": 30,
+                    "start_end": False
+                },
+                {
+                    "id": 8,
+                    "name": "Religia",
+                    "hours": 2,
+                    "teacher_id": 5,
+                    "start_end": True
+                },
+                {
+                    "id": 9,
+                    "name": "Informatyka",
+                    "hours": 1,
+                    "teacher_id": 18,
+                    "start_end": False
+                },
+                {
+                    "id": 10,
+                    "name": "J. Angielski",
+                    "hours": 2,
+                    "teacher_id": 43,
+                    "start_end": False
+                },
+                {
+                    "id": 11,
+                    "name": "Geografia",
+                    "hours": 1,
+                    "teacher_id": 39,
+                    "start_end": False
+                },
+                {
+                    "id": 12,
+                    "name": "Biologia",
+                    "hours": 1,
+                    "teacher_id": 37,
+                    "start_end": False
+                },
+                {
+                    "id": 13,
+                    "name": "G. Wychowawcza",
+                    "hours": 1,
+                    "teacher_id": 33,
+                    "start_end": False
+                },
+            ],
+            "5D": [
+                {
+                    "id": 1,
+                    "name": "J. Polski",
+                    "hours": 5,
+                    "teacher_id": 27,
+                    "start_end": False
+                },
+                {
+                    "id": 2,
+                    "name": "Historia",
+                    "hours": 2,
+                    "teacher_id": 38,
+                    "start_end": False
+                },
+                {
+                    "id": 3,
+                    "name": "Technika",
+                    "hours": 1,
+                    "teacher_id": 23,
+                    "start_end": False
+                },
+                {
+                    "id": 4,
+                    "name": "Matematyka",
+                    "hours": 5,
+                    "teacher_id": 42,
+                    "start_end": False
+                },
+                {
+                    "id": 5,
+                    "name": "WF",
+                    "hours": 4,
+                    "teacher_id": 44,
+                    "start_end": False
+                },
+                {
+                    "id": 6,
+                    "name": "Muzyka",
+                    "hours": 1,
+                    "teacher_id": 24,
+                    "start_end": False
+                },
+                {
+                    "id": 7,
+                    "name": "Plastyka",
+                    "hours": 1,
+                    "teacher_id": 30,
+                    "start_end": False
+                },
+                {
+                    "id": 8,
+                    "name": "Religia",
+                    "hours": 2,
+                    "teacher_id": 5,
+                    "start_end": True
+                },
+                {
+                    "id": 9,
+                    "name": "Informatyka",
+                    "hours": 1,
+                    "teacher_id": 18,
+                    "start_end": False
+                },
+                {
+                    "id": 10,
+                    "name": "J. Angielski",
+                    "hours": 2,
+                    "teacher_id": 43,
+                    "start_end": False
+                },
+                {
+                    "id": 11,
+                    "name": "Geografia",
+                    "hours": 1,
+                    "teacher_id": 39,
+                    "start_end": False
+                },
+                {
+                    "id": 12,
+                    "name": "Biologia",
+                    "hours": 1,
+                    "teacher_id": 37,
+                    "start_end": False
+                },
+                {
+                    "id": 13,
+                    "name": "G. Wychowawcza",
+                    "hours": 1,
+                    "teacher_id": 44,
+                    "start_end": False
                 },
             ],
         }

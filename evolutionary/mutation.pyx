acc64efd71f6448659f9a48ca73f93e6f830fab3
Robert Małafiejew <01169593@pw.edu.pl>
2024-12-12 20:01:05 +0100

    randomised preferred and other hours in mutation

--- evolutionary/mutation.pyx
+++ evolutionary/mutation.pyx
@@ -1,6 +1,6 @@
-from xmlrpc.client import boolean
-
 from libc.stdlib cimport rand
+
+ctypedef list[list[int]] Matrix
 
 cdef int H_PER_DAY = 8
 
@@ -8,14 +8,21 @@ cdef int HOURS = 5 * H_PER_DAY
 
 cdef int RAND_MAX = 32767
 
-cdef list PREFERRED_HOURS = [2, 3, 4, 5, 10, 11, 12, 13, 18, 19, 20, 21, 26, 27, 28, 29, 34, 35, 36, 37]
+cdef list[int] PREFERRED_HOURS = [2, 3, 4, 5, 10, 11, 12, 13, 18, 19, 20, 21, 26, 27, 28, 29, 34, 35, 36, 37]
 
-cdef list OTHER_HOURS = [0, 1, 6, 7, 8, 9, 14, 15, 16, 17, 22, 23, 24, 25, 30, 31, 32, 33, 38, 39]
+cdef list[int] OTHER_HOURS = [0, 1, 6, 7, 8, 9, 14, 15, 16, 17, 22, 23, 24, 25, 30, 31, 32, 33, 38, 39]
 
 cdef int random_hour():
     return rand() % HOURS
 
-ctypedef list[list[int]] Matrix
+cdef list[int] shuffle(list[int] arr):
+    cdef int n = len(arr)
+    cdef int i, j
+
+    for i in range(n-1, 0, -1):
+        j = rand() % (i + 1)
+        arr[i], arr[j] = arr[j], arr[i]
+    return arr
 
 cdef Matrix teacher_matrix(Matrix plan, list[dict] subject_to_teacher):
     cdef int rows = len(plan), cols = HOURS
@@ -39,16 +46,15 @@ cpdef Matrix mutate(Matrix plan, list[dict] subject_to_teacher):
     cdef int i, gid, lesson_h, rows = len(plan), cols = HOURS
     cdef int mutated = 0
 
-    cdef list preferred_hours = [i for i in range(HOURS)]
-
     teachers = teacher_matrix(plan, subject_to_teacher)
+    cdef list[int] pref_hours = shuffle(PREFERRED_HOURS), other_hours = shuffle(OTHER_HOURS)
 
     for gid in range(rows):
         lesson_h = random_hour()
         while plan[gid][lesson_h] == 0:
             lesson_h = random_hour()
 
-        for i in PREFERRED_HOURS:
+        for i in pref_hours:
             if count_teacher_lessons(teachers, teachers[gid][lesson_h], i) == 0 and plan[gid][i] == 0:
                 plan[gid][i] = plan[gid][lesson_h]
                 teachers[gid][i] = teachers[gid][lesson_h]
@@ -59,7 +65,7 @@ cpdef Matrix mutate(Matrix plan, list[dict] subject_to_teacher):
         if mutated == 1:
             continue
 
-        for i in OTHER_HOURS:
+        for i in other_hours:
             if count_teacher_lessons(teachers, teachers[gid][lesson_h], i) == 0 and plan[gid][i] == 0:
                 plan[gid][i] = plan[gid][lesson_h]
                 teachers[gid][i] = teachers[gid][lesson_h]

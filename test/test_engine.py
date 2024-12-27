9e0b4cb60e030b4d6228c5f8c93945c8780e5c73
Robert Małafiejew <01169593@pw.edu.pl>
2024-12-27 19:26:47 +0100

    incomplete test written

--- test/test_engine.py
+++ test/test_engine.py
@@ -1,11 +1,14 @@
+import json
 import time
 
 from matplotlib import pyplot as plt
 
-from evolutionary.config import MetaConfig, Config
+from evolutionary.config import MetaConfig, Config, EvaluationCriteria, CrossParams
 from evolutionary.generation import Generation
+from evolutionary.selection import RouletteSelection
 
-NO_GENERATIONS = 1_000
+NO_GENERATIONS = 1_000 # Number of generations algorithm should go through
+
 
 def plot_graph(stats, file_name):
     x = [s["gen"] for s in stats]
@@ -24,6 +27,7 @@ def plot_graph(stats, file_name):
 
     plt.savefig(file_name + ".png", bbox_inches="tight")
 
+
 def run_engine(gen: Generation) -> list[dict]:
     stats = []
     gen.evaluate()
@@ -33,19 +37,78 @@ def run_engine(gen: Generation) -> list[dict]:
 
         if gen.gen_no % 50 == 0:
             stats.append(gen.statistics())
+            print(gen.gen_no) # FIXME: delete when ok
     return stats
 
+
 if __name__ == "__main__":
-    mconfig = MetaConfig() # TODO: add config
+    # Loading 1st data set
+    sf = open("test_sets/subjects_1.json", "r")
+    tf = open("test_sets/teachers_1.json", "r")
+
+    # Configuration for most "basic" algorithm
+    mconfig = MetaConfig(
+        population_size=100,
+        selection=RouletteSelection(),
+        teachers=json.load(tf),
+        subjects=json.load(sf),
+        eval_criteria=EvaluationCriteria(
+            hours_weight=[1, 1, 1, 1, 1, 1, 1, 1],
+            basic_importance=1,
+            blank_lessons_importance=1,
+            hours_per_day_importance=1,
+            max_subject_hours_per_day_importance=1,
+            subject_block_importance=1,
+            teacher_block_importance=1,
+            subject_at_end_or_start_importance=1,
+        ),
+        cross_params=CrossParams(
+            crossover_rate=0.8,
+            mutation_rate=0.01
+        ),
+        c=1.5
+    )
+
+    # Setup
     config = Config(mconfig)
     generation = Generation(config, mconfig)
 
+    # Engine run
     start = time.time()
     results = run_engine(generation)
     end = time.time()
 
     delta = end - start
 
+    # Results of test one
     plot_graph(results, "test_1")
-
     print(f"Time spent in first example: {delta} seconds")
+
+
+def test2():
+    # Loading 1st data set
+    sf = open("test_sets/subjects_2.json", "r")
+    tf = open("test_sets/teachers_2.json", "r")
+
+    # Configuration with other selection method
+    mconfig = MetaConfig(
+        population_size=100,
+        selection=RouletteSelection(),
+        teachers=json.load(tf),
+        subjects=json.load(sf),
+        eval_criteria=EvaluationCriteria(
+            hours_weight=[1, 1, 1, 1, 1, 1, 1, 1],
+            basic_importance=1,
+            blank_lessons_importance=1,
+            hours_per_day_importance=1,
+            max_subject_hours_per_day_importance=1,
+            subject_block_importance=1,
+            teacher_block_importance=1,
+            subject_at_end_or_start_importance=1,
+        ),
+        cross_params=CrossParams(
+            crossover_rate=0.8,
+            mutation_rate=0.01
+        ),
+        k=4
+    )

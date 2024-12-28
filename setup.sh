ea3df925cd93b388a6591443e9598134f6a25bac
Robert Małafiejew <01169593@pw.edu.pl>
2024-12-28 19:07:28 +0100

    comments fix

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
-# Clear previously generated libraries and create new
-# Needs to be in venv!!!
+# Clear previously generated C code and  shared objects, and create new
+# Needs to be in a venv!
 python3 setup.py clean --all
 rm -rf build evolutionary/*.so evolutionary/*.c
 python3 setup.py build_ext --inplace

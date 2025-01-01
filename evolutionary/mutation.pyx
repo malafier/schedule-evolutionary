e2b2a291aefb48dd148b9c1fd366f0878f09c1d4
Robert Małafiejew <01169593@pw.edu.pl>
2025-01-01 19:49:31 +0100

    algorithm changed

--- evolutionary/mutation.pyx
+++ evolutionary/mutation.pyx
@@ -9,10 +9,6 @@ cdef int HOURS = 5 * H_PER_DAY
 
 cdef int RAND_MAX = 32767
 
-cdef list[int] PREFERRED_HOURS = [2, 3, 4, 5, 10, 11, 12, 13, 18, 19, 20, 21, 26, 27, 28, 29, 34, 35, 36, 37]
-
-cdef list[int] OTHER_HOURS = [0, 1, 6, 7, 8, 9, 14, 15, 16, 17, 22, 23, 24, 25, 30, 31, 32, 33, 38, 39]
-
 cdef int random_hour():
     return rand() % HOURS
 
@@ -22,7 +18,7 @@ cdef list[int] shuffle(list[int] arr):
     cdef int n = len(arr)
     cdef int i, j
 
-    for i in range(n-1, 0, -1):
+    for i in range(n - 1, 0, -1):
         j = rand() % (i + 1)
         arr[i], arr[j] = arr[j], arr[i]
     return arr
@@ -56,25 +52,14 @@ cpdef Matrix mutate(Matrix plan, list[dict] subject_to_teacher):
     cdef int mutated = 0
 
     teachers = teacher_matrix(plan, subject_to_teacher)
-    cdef list[int] pref_hours = shuffle(PREFERRED_HOURS), other_hours = shuffle(OTHER_HOURS)
+    cdef list[int] rand_hours = shuffle([x for x in range(HOURS)])
 
     for gid in range(rows):
         lesson_h = random_hour()
         while plan[gid][lesson_h] == 0:
             lesson_h = random_hour()
 
-        for i in pref_hours:
-            if count_teacher_lessons(teachers, teachers[gid][lesson_h], i) == 0 and plan[gid][i] == 0:
-                plan[gid][i] = plan[gid][lesson_h]
-                teachers[gid][i] = teachers[gid][lesson_h]
-                plan[gid][lesson_h] = 0
-                teachers[gid][lesson_h] = 0
-                mutated = 1
-                break
-        if mutated == 1:
-            continue
-
-        for i in other_hours:
+        for i in rand_hours:
             if count_teacher_lessons(teachers, teachers[gid][lesson_h], i) == 0 and plan[gid][i] == 0:
                 plan[gid][i] = plan[gid][lesson_h]
                 teachers[gid][i] = teachers[gid][lesson_h]
